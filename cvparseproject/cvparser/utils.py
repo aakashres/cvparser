69d1908e5dab6cb113707fd03f190fdf05efbd06
Aakash Shrestha <asha@logpoint.com>
2020-01-15 18:28:53 +0545

    Added few imports for proper cv parsing. Added docstrings and error handling

--- cvparseproject/cvparser/utils.py
+++ cvparseproject/cvparser/utils.py
@@ -1,13 +1,35 @@
 import os
+import en_core_web_sm
+
 from django.conf import settings
 from pyresparser import ResumeParser
 
+nlp = en_core_web_sm.load()
+
 
 def handle_uploaded_file(cv):
+    """
+        Extracts skills mentioned in a CV.
+
+        Saves uploaded CV in a temporary location and uses
+        pyresparser to extract all necessary informatiion from
+        that CV. After extraction, Uploaded file is deleted.
+
+        Parameters:
+        cv (file): CV uploaded form UI
+
+        Returns:
+        list of string: List of skills extracted from CV. Might be empty list
+                        if it is unable to extract skills
+    """
     upload_path = os.path.join(settings.BASE_DIR, "uploads", cv.name)
     with open(upload_path, "wb+") as destination:
         for chunk in cv.chunks():
             destination.write(chunk)
-    data = ResumeParser(upload_path).get_extracted_data()
-    os.remove(upload_path)
-    return data
+    try:
+        data = ResumeParser(upload_path).get_extracted_data()
+    except Exception:
+        data = {"skills": []}
+    finally:
+        os.remove(upload_path)
+    return data.get("skills", [])
