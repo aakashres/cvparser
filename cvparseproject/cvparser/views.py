dd4ea02877b0a45fef08e244b6b5eba60dc2f292
Aakash Shrestha <asha@logpoint.com>
2020-01-15 18:29:59 +0545

    Formattiing and returning error in case of invalid files

--- cvparseproject/cvparser/views.py
+++ cvparseproject/cvparser/views.py
@@ -9,17 +9,22 @@ from .forms import CVForm
 
 class UploadView(View):
     def get(self, request):
-        context = {"form": CVForm(), "skills": None, "process_time": None}
+        context = {"form": CVForm()}
         return render(request, "fileupload.html", context)
 
     def post(self, request):
-        data = None
         form = CVForm(request.POST or None, request.FILES or None)
         context = {"form": form}
-        cvfile = request.FILES.get("cv")
-        if cvfile.name.split(".")[-1].lower() in ["pdf", "doc"]:
+        cvfile = request.FILES.get("cv", None)
+        if cvfile and cvfile.name.split(".")[-1].lower() in ["pdf", "doc",  "docx"]:
             start_time = time.time()
-            data = handle_uploaded_file(cvfile)
+            skills = handle_uploaded_file(cvfile)
             end_time = time.time()
-            context.update({"skills": data.get("skills"), "process_time": end_time - start_time})
+            context.update(
+                {"skills": skills, "process_time": end_time - start_time}
+            )
+            if not skills:
+                context.update({"message": "Cannot Extract skills form your CV."})
+        else:
+            context.update({"message": "Error processing your CV. Please reupload it"})
         return render(request, "fileupload.html", context)
